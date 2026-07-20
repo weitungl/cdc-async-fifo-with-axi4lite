ca73f9859a36ed844603586d54ff5874db20d75d
Wei-Tung Lai <weitungl@caen-vnc-mi14.engin.umich.edu>
2026-07-20 05:20:23 -0400

    optimize rptr_handler

--- rtl/rptr_handler.sv
+++ rtl/rptr_handler.sv
@@ -13,21 +13,25 @@ module rptr_handler #(
     output logic [WIDTH-1:0]    g_rptr,
     output logic [WIDTH-1:0]    b_rptr
 );
+    logic [WIDTH-1:0]   b_rptr_internal;
     logic [WIDTH-1:0]   b_rptr_next;
     logic [WIDTH-1:0]   g_rptr_next;
 
-    assign b_rptr_next = (r_en && !empty)? b_rptr + 1'b1 : b_rptr;
+    assign b_rptr_next = (r_en && !empty)? b_rptr_internal + 1'b1 : b_rptr_internal;
     assign g_rptr_next =  b_rptr_next ^ (b_rptr_next >> 1);
 
-    always_ff @(posedge rclk, negedge rrst_n)begin
-        if(~rrst_n)begin
-            b_rptr <= '0;
-            g_rptr <= '0;
+    always_ff @(posedge rclk, negedge rrst_n) begin
+        if(~rrst_n) begin
+            b_rptr_internal <= '0;  
+            b_rptr          <= '0;  
+            g_rptr          <= '0;
         end else begin
-            b_rptr <= b_rptr_next;
-            g_rptr <= g_rptr_next;
+            b_rptr_internal <= b_rptr_next; 
+            b_rptr          <= b_rptr_next; 
+            g_rptr          <= g_rptr_next;
         end
     end
+
 
     assign empty_next = (g_rptr_next == g_wptr_sync);
 
